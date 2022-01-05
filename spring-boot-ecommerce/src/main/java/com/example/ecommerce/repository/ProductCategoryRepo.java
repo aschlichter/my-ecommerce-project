89506d84ca90ec889eb0af2f32c6edc68d2bdf4e
aschlichter <adipostaja@gmail.com>
2022-01-05 09:03:35 +0100

    CrossOrigin added to repository classes

--- spring-boot-ecommerce/src/main/java/com/example/ecommerce/repository/ProductCategoryRepo.java
+++ spring-boot-ecommerce/src/main/java/com/example/ecommerce/repository/ProductCategoryRepo.java
@@ -3,10 +3,12 @@ package com.example.ecommerce.repository;
 import com.example.ecommerce.model.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * Created by adam on 2022. 01. 04.
  */
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepo extends JpaRepository<ProductCategory, Long> {
 }
--- spring-boot-ecommerce/src/main/java/com/example/ecommerce/repository/ProductRepo.java
+++ spring-boot-ecommerce/src/main/java/com/example/ecommerce/repository/ProductRepo.java
@@ -2,9 +2,11 @@ package com.example.ecommerce.repository;
 
 import com.example.ecommerce.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * Created by adam on 2022. 01. 04.
  */
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepo extends JpaRepository<Product, Long> {
 }
