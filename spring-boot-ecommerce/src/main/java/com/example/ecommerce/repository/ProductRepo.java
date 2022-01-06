06aead934261a11f4bda33e43b3eb05f75049cbf
aschlichter <adipostaja@gmail.com>
2022-01-06 11:19:29 +0100

    Spring Product Repository updated

--- spring-boot-ecommerce/src/main/java/com/example/ecommerce/repository/ProductRepo.java
+++ spring-boot-ecommerce/src/main/java/com/example/ecommerce/repository/ProductRepo.java
@@ -1,12 +1,17 @@
 package com.example.ecommerce.repository;
 
 import com.example.ecommerce.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by adam on 2022. 01. 04.
  */
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepo extends JpaRepository<Product, Long> {
+
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 }
