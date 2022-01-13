3cbe232c240f42f8ae4d1eff71ae2e707d6d530e
aschlichter <adipostaja@gmail.com>
2022-01-13 12:34:33 +0100

    Country and State repos created

---
+++ spring-boot-ecommerce/src/main/java/com/example/ecommerce/repository/StateRepo.java
@@ -0,0 +1,19 @@
+package com.example.ecommerce.repository;
+
+import com.example.ecommerce.model.State;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.List;
+
+/**
+ * Created by adam on 2022. 01. 13.
+ */
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource
+public interface StateRepo extends JpaRepository<State, Integer> {
+
+    List<State> findByCountryCode(@Param("code") String code);
+}
