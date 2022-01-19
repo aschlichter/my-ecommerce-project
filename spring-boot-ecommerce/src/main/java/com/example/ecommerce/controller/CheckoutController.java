25a06d87a480e79be303e54013efc84e711bf197
aschlichter <adipostaja@gmail.com>
2022-01-19 15:39:59 +0100

    Checkout controller created

---
+++ spring-boot-ecommerce/src/main/java/com/example/ecommerce/controller/CheckoutController.java
@@ -0,0 +1,29 @@
+package com.example.ecommerce.controller;
+
+import com.example.ecommerce.dto.Purchase;
+import com.example.ecommerce.dto.PurchaseResponse;
+import com.example.ecommerce.service.CheckoutService;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by adam on 2022. 01. 19.
+ */
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+
+    private CheckoutService checkoutService;
+
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+
+        return purchaseResponse;
+    }
+}
