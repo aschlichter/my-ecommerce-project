c4a5c890346cb1dbf22cb3474045d5524bc1e50d
aschlichter <adipostaja@gmail.com>
2022-01-21 11:25:12 +0100

    OIDC config created

---
+++ angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+  oidc: {
+    clientId: '0oa3o3i03vk9UkM045d7',
+    issuer: '***REMOVED***',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email']
+  }
+
+}
