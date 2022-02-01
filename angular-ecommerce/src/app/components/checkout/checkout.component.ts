b80cf7cc642d050d63dabb310113faf791a6fc1b
aschlichter <adipostaja@gmail.com>
2022-02-01 12:13:06 +0100

    Prepopulate email form

--- angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -31,6 +31,7 @@ export class CheckoutComponent implements OnInit {
   shippingAddressStates: State[] = [];
   billingAddressStates: State[] = [];
 
+  storage: Storage = sessionStorage;
 
   constructor(private formBuilder: FormBuilder,
               private myWebShopFormService: MyWebShopFormService,
@@ -42,6 +43,8 @@ export class CheckoutComponent implements OnInit {
 
     this.reviewCartDetails();
 
+    const theEmail = JSON.parse(this.storage.getItem('userEmail'));
+
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
         firstName: new FormControl('',
@@ -52,7 +55,7 @@ export class CheckoutComponent implements OnInit {
                                   [Validators.required, 
                                    Validators.minLength(2), 
                                    MyWebShopValidators.notOnlyWhiteSpace]),
-        email: new FormControl('',
+        email: new FormControl(theEmail,
                                [Validators.required, 
                                 Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
       }),
