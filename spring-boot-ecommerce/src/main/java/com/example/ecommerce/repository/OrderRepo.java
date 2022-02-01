f5a99eae957186713e15430f702fa4f1ee1ce5c2
aschlichter <adipostaja@gmail.com>
2022-02-01 12:02:48 +0100

    Sorting orders by date added

--- spring-boot-ecommerce/src/main/java/com/example/ecommerce/repository/OrderRepo.java
+++ spring-boot-ecommerce/src/main/java/com/example/ecommerce/repository/OrderRepo.java
@@ -13,5 +13,5 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource
 public interface OrderRepo extends JpaRepository<Order, Long> {
 
-    Page<Order> findByCustomerEmail(@Param("email") String email, Pageable pageable);
+    Page<Order> findByCustomerEmailOrderByDateCreatedDesc(@Param("email") String email, Pageable pageable);
 }
