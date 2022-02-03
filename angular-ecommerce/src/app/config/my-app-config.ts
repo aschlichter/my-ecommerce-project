fa662207c5f758c505e7b1b37525ce22208451ed
aschlichter <adipostaja@gmail.com>
2022-02-03 10:57:56 +0100

    added HTTPS to redirect URI

--- angular-ecommerce/src/app/config/my-app-config.ts
+++ angular-ecommerce/src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
   oidc: {
     clientId: '0oa3o3i03vk9UkM045d7',
     issuer: '***REMOVED***',
-    redirectUri: 'http://localhost:4200/login/callback',
+    redirectUri: 'https://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email']
   }
 
