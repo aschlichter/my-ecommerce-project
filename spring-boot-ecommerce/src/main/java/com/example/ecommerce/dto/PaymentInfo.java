b7ba9c307398573c164ce7e033dc4bac1ce7da99
aschlichter <adipostaja@gmail.com>
2022-02-05 10:19:02 +0100

    PaymentInfo dto class created

---
+++ spring-boot-ecommerce/src/main/java/com/example/ecommerce/dto/PaymentInfo.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce.dto;
+
+import lombok.Data;
+
+/**
+ * Created by adam on 2022. 02. 05.
+ */
+@Data
+public class PaymentInfo {
+
+    private int amount;
+    private String currency;
+}
