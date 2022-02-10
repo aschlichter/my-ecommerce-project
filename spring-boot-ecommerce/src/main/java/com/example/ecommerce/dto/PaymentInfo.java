1c665d5349e125d589845f45ac0c8b8f1085ad1a
aschlichter <adipostaja@gmail.com>
2022-02-10 10:51:01 +0100

    Added Stripe email receipt for testing

--- spring-boot-ecommerce/src/main/java/com/example/ecommerce/dto/PaymentInfo.java
+++ spring-boot-ecommerce/src/main/java/com/example/ecommerce/dto/PaymentInfo.java
@@ -10,4 +10,5 @@ public class PaymentInfo {
 
     private int amount;
     private String currency;
+    private String receiptEmail;
 }
--- spring-boot-ecommerce/src/main/java/com/example/ecommerce/service/CheckoutServiceImpl.java
+++ spring-boot-ecommerce/src/main/java/com/example/ecommerce/service/CheckoutServiceImpl.java
@@ -72,6 +72,7 @@ public class CheckoutServiceImpl implements CheckoutService {
         params.put("currency", paymentInfo.getCurrency());
         params.put("payment_method_types", paymentMethodTypes);
         params.put("description", "MyWebShop purchase");
+        params.put("receipt_email", paymentInfo.getReceiptEmail());
 
         return PaymentIntent.create(params);
     }
